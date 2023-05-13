4675afc53b3900faf7c85f393d42b6104adbd8f8
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-05-13 13:56:12 +0300

    configure dap

--- config.lua
+++ config.lua
@@ -121,6 +121,106 @@ require('fine-cmdline').setup({
   },
 })
 
+local dap = require('dap')
+dap.adapters.chrome = {
+  type = "executable",
+  command = "node",
+  runtimeExecutable = "google-chrome-stable",
+  args = {
+    vim.fn.stdpath("data") .. "/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js",
+  },
+}
+dap.adapters.node2 = {
+  type = "executable",
+  command = "node",
+  args = {
+    vim.fn.stdpath("data") .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js",
+  },
+}
+
+dap.adapters['pwa-node'] = {
+  type = "executable",
+  command = "node",
+  args = {
+    vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+  },
+}
+
+dap.configurations.typescript = {
+  {
+    type = "node2",
+    name = "node attach",
+    request = "attach",
+    program = "${file}",
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+  },
+  {
+    type = "node2",
+    name = "node launch file",
+    request = "launch",
+    runtimeExecutable = "node",
+    runtimeArgs = { "--nolazy", "-r", "ts-node/register" },
+    args = { "${file}" },
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+    skipFiles = { "<node_internals>/**", "node_modules/**" },
+  },
+  {
+    type = "chrome",
+    name = "chrome",
+    request = "attach",
+    program = "${file}",
+    -- cwd = "${workspaceFolder}",
+    -- protocol = "inspector",
+    port = 9222,
+    webRoot = "${workspaceFolder}",
+    -- sourceMaps = true,
+    sourceMapPathOverrides = {
+      -- Sourcemap override for nextjs
+      ["webpack://_N_E/./*"] = "${webRoot}/*",
+      ["webpack:///./*"] = "${webRoot}/*",
+    },
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach",
+    processId = require 'dap.utils'.pick_process,
+    cwd = "${workspaceFolder}",
+  },
+}
+
+dap.configurations.typescriptreact = {
+  {
+    type = "chrome",
+    name = "chrome attach",
+    request = "attach",
+    program = "${file}",
+    -- cwd = "${workspaceFolder}",
+    -- protocol = "inspector",
+    port = 9222,
+    webRoot = "${workspaceFolder}",
+    -- sourceMaps = true,
+    sourceMapPathOverrides = {
+      -- Sourcemap override for nextjs
+      ["webpack://_N_E/./*"] = "${webRoot}/*",
+      ["webpack:///./*"] = "${webRoot}/*",
+    },
+  },
+  {
+    type = "node2",
+    name = "node attach",
+    request = "attach",
+    program = "${file}",
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+  },
+}
+
 ---- PLUGINS ----
 lvim.plugins = {
   {
@@ -145,5 +245,5 @@ lvim.plugins = {
     dependencies = {
       { 'MunifTanjim/nui.nvim' }
     }
-  }
+  },
 }
